54aaa57dce3435cef0fa39836e1c6bfcbec71193
Hebi Li <lihebi.com@gmail.com>
2019-09-28 21:56:40 -0500

    use info.rkt

---
+++ info.rkt
@@ -0,0 +1,8 @@
+#lang info
+
+(define version "0.1")
+(define deps (list "sxml"
+                   "html-parsing"
+                   "roman-numeral"))
+(define pkg-desc "generate biber by parsing conference website")
+(define pkg-authors '(lihebi))
